8e0714d5a6ceddc1b52c31c17128285881947ee9
luke <luke.edwards05@gmail.com>
2016-01-21 01:14:06 -0800

    install templatecache & configure

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,36 @@
 const fanny = require('fannypack');
 const C = fanny.Config;
 
+require('fannypack-angular-templatecache');
+
 C.root = {
   src: './src',
   dest: './build'
 };
 
-C.js.src = 'scripts';
+C.js.src = ''; // root.src
 C.js.dest = 'assets/js';
 C.js.entries = {
-  app: ['./app.js']
+  main: ['./main.js']
 };
 
-C.sass.src = 'styles';
+C.sass.src = ''; // root.src
 C.sass.dest = 'assets/css';
 
-C.html.src = '';
+C.html.src = 'html';
+
+C.ngTemplates = {
+	src: 'app',
+	dest: 'assets/js',
+	watchTask: true,
+	codeTask: true,
+	options: {
+		module: 'app.templates',
+		standalone: true
+	},
+	extensions: ['html']
+};
+
 C.browsersync = {
   server: {
     baseDir: C.root.dest
